ee33de44438f1ebce815182a2d15d37ec0011ac0
William Nogueira <wyllyam11@hotmail.com>
2026-03-11 00:25:05 -0300

    Classes Atividade e Categoria

--- src/main/java/com/devsuperior/sistema_evento_desafio/entities/Categoria.java
+++ src/main/java/com/devsuperior/sistema_evento_desafio/entities/Categoria.java
@@ -1,8 +1,25 @@
 package com.devsuperior.sistema_evento_desafio.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> list = new ArrayList<>();
+
+    public Categoria() {
+    }
 
     public Categoria(Integer id, String descricao) {
         this.id = id;
